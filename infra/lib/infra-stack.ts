e5980775eee0bdd25b37af706e5f004187e98386
Andre Sasser <sasseran@msu.edu>
2026-07-06 22:25:06 -0400

    serverless event dashboard code

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,5 +1,8 @@
+import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 
 export class InfraStack extends cdk.Stack {
@@ -17,7 +20,18 @@ export class InfraStack extends cdk.Stack {
       billingMode: BillingMode.PAY_PER_REQUEST,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
+    
+    const ingestEventFunction = new NodejsFunction(this, 'IngestEventFunction', {
+    functionName: 'serverless-event-dashboard-ingest-event',
+    entry: path.join(__dirname, '../../backend/functions/ingest-event/index.ts'),
+    projectRoot: path.join(__dirname, '../..'),
+    depsLockFilePath: path.join(__dirname, '../package-lock.json'),
+    handler: 'handler',
+    runtime: Runtime.NODEJS_22_X,
+    environment: {
+      EVENTS_TABLE_NAME: this.eventsTable.tableName,
+    },
+  });
+    this.eventsTable.grantWriteData(ingestEventFunction);
   }
 }
-
-
